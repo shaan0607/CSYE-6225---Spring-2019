d3483c5e236eb5568c2bf1c63463e3bbdf7d38f8
Ritu Agrawal <agrawal.ritu@husky.neu.edu>
2019-02-21 16:06:45 -0500

    Application Stack

---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -0,0 +1,100 @@
+echo "Please enter Network Stack Name:"
+read networkStackName
+if [ -z "$networkStackName" ]
+then
+	echo "StackName error exiting!"
+	exit 1
+fi
+echo "$networkStackName"
+
+
+
+echo "Please enter Application Stack Name:"
+read appStackName
+if [ -z "$appStackName" ]
+then
+	echo "StackName error exiting!"
+	exit 1
+fi
+echo "$appStackName"
+
+
+
+echo "Please enter an active keyPair to associate with EC2:"
+read keyName
+if [ -z "$keyName" ]
+then
+	echo "StackName error exiting!"
+	exit 1
+fi
+echo "$keyName"
+
+echo "Please enter the ImageID of centos AMI  created"
+read imageid
+if [ -z "$imageid" ]
+then
+	echo "ImageID error \n Exiting"
+	exit 1
+fi
+
+VpcId=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' \
+--filters "Name=tag:Name,Values=$networkStackName-csye6225-vpc" "Name=is-default, Values=false" --output text 2>&1)
+
+
+subnet=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${VpcId})
+subnetid1=$(echo -e "$subnet" | jq '.Subnets[0].SubnetId' | tr -d '"')
+subnetid2=$(echo -e "$subnet" | jq '.Subnets[1].SubnetId' | tr -d '"')
+subnetid3=$(echo -e "$subnet" | jq '.Subnets[2].SubnetId' | tr -d '"')
+
+
+
+if [ -z "$subnetid1" ]
+then
+	echo "Subnet ID 1 error \n Exiting"
+	exit 1
+fi
+
+
+if [ -z "$VpcId" ]
+then
+	echo "VPC ID error \n Exiting"
+	exit 1
+fi
+
+
+if [ -z "$subnetid2" ]
+then
+	echo "Subnet ID 2 error \n Exiting"
+	exit 1
+fi
+
+
+if [ -z "$subnetid3" ]
+then
+	echo "Subnet ID 3 error \n Exiting"
+	exit 1
+fi
+
+
+
+# Create CloudFormation Stack
+echo "Validating template"
+TMP_code=`aws cloudformation validate-template --template-body file://./csye-cf-application.json`
+if [ -z "$TMP_code" ]
+then
+	echo "Template error exiting!"
+	exit 1
+fi
+echo "Cloudformation template validation success"
+
+echo "Now Creating CloudFormation Stack"
+
+CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye-cf-application.json --parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$networkStackName ParameterKey=ApplicationStackNameParameter,ParameterValue=$appStackName ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=VpcID,ParameterValue=$VpcId ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3 ParameterKey=ImageID,ParameterValue=$imageid`
+if [ -z "$CRTSTACK_Code" ]
+then
+	echo "Stack Creation error exiting!"
+	exit 1
+fi
+aws cloudformation wait stack-create-complete --stack-name $appStackName
+echo "Application Stack Created"
+echo "Check AWS Cloudformation"
