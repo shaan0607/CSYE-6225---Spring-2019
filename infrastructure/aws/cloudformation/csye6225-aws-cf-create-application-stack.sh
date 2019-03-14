2d84a24b17230bfecea7654a820f8fc3379c2a51
Shaan0607 <deosthale.s@northeastern.edu>
2019-03-14 03:34:34 -0400

    hello

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -29,15 +29,24 @@ then
 fi
 echo "$keyName"
 
-echo "Please enter the ImageID of centos AMI  created"
-read imageid
-if [ -z "$imageid" ]
+#echo "Please enter the ImageID of centos AMI  created"
+#read imageid
+echo "Your latest AMI ID is:"
+imageid=$(aws ec2 describe-images --filters "Name=name,Values=csye6225*" --query "sort_by(Images, &CreationDate)[-1].[ImageId]" --output "text")
+if [ $? -eq 0 ]
 then
-	echo "ImageID error \n Exiting"
-	exit 1
+        echo "$imageid"
+else
+        echo "AMI Id retrival Failed"
+        exit 0
 fi
 
-VpcId=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' --filters "Name=tag:Name,Values=$networkStackName-csye6225-vpc" "Name=is-default, Values=false" --output text 2>&1)
+
+
+
+
+VpcId=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' \
+--filters "Name=tag:Name,Values=$networkStackName-csye6225-vpc" "Name=is-default, Values=false" --output text 2>&1)
 
 
 subnet=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${VpcId})
@@ -74,43 +83,6 @@ then
 	exit 1
 fi
 
-
-VpcId=$(aws ec2 describe-vpcs --query 'Vpcs[].{VpcId:VpcId}' --filters "Name=tag:Name,Values=$networkStackName-csye6225-vpc" "Name=is-default, Values=false" --output text 2>&1)
-
-
-subnet=$(aws ec2 describe-subnets --filters Name=vpc-id,Values=${VpcId})
-subnetid1=$(echo -e "$subnet" | jq '.Subnets[0].SubnetId' | tr -d '"')
-subnetid2=$(echo -e "$subnet" | jq '.Subnets[1].SubnetId' | tr -d '"')
-subnetid3=$(echo -e "$subnet" | jq '.Subnets[2].SubnetId' | tr -d '"')
-
-
-
-if [ -z "$subnetid1" ]
-then
-	echo "Subnet ID 1 error \n Exiting"
-	exit 1
-fi
-
-
-if [ -z "$VpcId" ]
-then
-	echo "VPC ID error \n Exiting"
-	exit 1
-fi
-
-
-if [ -z "$subnetid2" ]
-then
-	echo "Subnet ID 2 error \n Exiting"
-	exit 1
-fi
-
-
-if [ -z "$subnetid3" ]
-then
-	echo "Subnet ID 3 error \n Exiting"
-	exit 1
-fi
 
 
 # Create CloudFormation Stack
@@ -125,9 +97,10 @@ echo "Cloudformation template validation success"
 
 echo "Now Creating CloudFormation Stack"
 
-ARN=$(aws iam get-role --role-name CodeDeployServiceRole --query 'Role.Arn' --output text)
+export circleciuser=circleci
 
-CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$networkStackName ParameterKey=ApplicationStackNameParameter,ParameterValue=$appStackName ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=VpcID,ParameterValue=$VpcId ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3 ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=CDAPPNAME,ParameterValue=csye6225-webapp ParameterKey=CDGRPNAME,ParameterValue=csye6225-webapp-deployment ParameterKey=ARN,ParameterValue=$ARN`
+CRTSTACK_Code=`aws cloudformation create-stack --stack-name $appStackName --template-body file://./csye6225-cf-application.json --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=NetworkStackNameParameter,ParameterValue=$networkStackName ParameterKey=ApplicationStackNameParameter,ParameterValue=$appStackName  ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=VpcID,ParameterValue=$VpcId ParameterKey=PublicSubnetKey1,ParameterValue=$subnetid1 ParameterKey=PublicSubnetKey2,ParameterValue=$subnetid2 ParameterKey=PublicSubnetKey3,ParameterValue=$subnetid3 ParameterKey=ImageID,ParameterValue=$imageid ParameterKey=circleci,ParameterValue=$circleciuser`
+
 if [ -z "$CRTSTACK_Code" ]
 then
 	echo "Stack Creation error exiting!"
---
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-policies.sh
@@ -0,0 +1,27 @@
+echo Enter Stack name
+#read stack name
+read sn
+#create stack
+{
+  validresp=$(aws cloudformation validate-template --template-body file://csye6225-aws-cf-policies.json) &&
+  echo "Template validated"
+} || {
+  echo "$validresp"
+  echo "Invalid Template"
+  exit 1
+}
+export circleciuser=circleci
+
+DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+
+CD_DOMAIN="code-deploy."${DOMAIN_NAME%?}
+
+createres=$(aws cloudformation create-stack  --stack-name $sn --capabilities CAPABILITY_NAMED_IAM --template-body file://csye6225-aws-cf-policies.json  --parameters ParameterKey=circleci,ParameterValue=$circleciuser ParameterKey=CDARN,ParameterValue=arn:aws:s3:::$CD_DOMAIN)
+echo Creating stack "$sn". Please wait...
+resp=$(aws cloudformation wait stack-create-complete --stack-name $sn)
+if [[ -z "$resp" ]]; then
+  echo Stack "$sn" sucessfully created
+else
+  echo "$resp"
+  exit 1
+fi
