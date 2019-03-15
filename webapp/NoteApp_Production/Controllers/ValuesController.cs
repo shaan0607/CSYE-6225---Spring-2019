8d01089fef412f10997a643e636014336e7fc457
Ritu Agrawal <agrawal.ritu@husky.neu.edu>
2019-03-14 21:06:59 -0400

    files

--- webapp/NoteApp_Production/Controllers/ValuesController.cs
+++ webapp/NoteApp_Production/Controllers/ValuesController.cs
@@ -304,6 +304,7 @@ namespace trial.Controllers
 
                   string username = getUsername();
                   NOTES note = _context.notes.Find(id);
+                  
                   if(note.EMAIL == username){
                   var ID = note.noteID;
                 //IEnumerable<Attachments> a = _context.attachments.AsEnumerable();
