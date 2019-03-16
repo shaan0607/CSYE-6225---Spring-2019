e983f91e17ac7671056da858c4cefd4308526946
Ritu Agrawal <agrawal.ritu@husky.neu.edu>
2019-03-15 21:31:12 -0400

    files

--- infrastructure/aws/cloudformation/csye6225-aws-cf-policies.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-policies.sh
@@ -1,12 +1,10 @@
 
-31 lines (26 sloc) 1.24 KB
 echo Enter Stack name
 #read stack name
 read sn
 #create stack
 {
-  validresp=$(aws cloudformation validate-template --template-body file://csye6225-aws-cf-policies.json) &&
-  echo "Template validated"
+  validresp=$(aws cloudformation validate-template --template-body file://csye6225-aws-cf-policies.json) &&  echo "Template validated"
 } || {
   echo "$validresp"
   echo "Invalid Template"
