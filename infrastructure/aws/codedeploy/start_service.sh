1610e883eae0131a05f574790b26b6ca567044a7
Ritu Agrawal <agrawal.ritu@husky.neu.edu>
2019-03-20 22:06:44 -0400

    Newtest

--- infrastructure/aws/codedeploy/start_service.sh
+++ infrastructure/aws/codedeploy/start_service.sh
@@ -4,3 +4,5 @@ cd /home/centos/WebApp/
 # use systemd to start and monitor dotnet application
 sudo systemctl enable kestrel.service
 sudo systemctl start kestrel.service
+sudo rm -rf /home/centos/WebApp/logs/*
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/WebApp/cloudwatch-config.json -s
