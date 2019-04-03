7fcdc7568dbf707e97e382ccf89af0af44c17966
Shaan0607 <deosthale.s@northeastern.edu>
2019-04-03 03:22:18 -0400

    HI

--- webapp/NoteApp_Production/Program.cs
+++ webapp/NoteApp_Production/Program.cs
@@ -31,7 +31,7 @@ namespace NoteApp_Production
                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
                     options.Limits.MinResponseDataRate =
                         new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-                    options.Listen(IPAddress.Any, 5000,listenOptions => {listenOptions.UseHttps();});
+                    options.Listen(IPAddress.Any, 5000);
                      
                   
                 })
