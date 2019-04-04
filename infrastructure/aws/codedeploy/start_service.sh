de81e89e6d8c1e6b9bce1fb4866a8e972861c657
Shaan0607 <deosthale.s@northeastern.edu>
2019-04-04 13:46:09 -0400

    BYE

--- infrastructure/aws/codedeploy/start_service.sh
+++ infrastructure/aws/codedeploy/start_service.sh
@@ -4,7 +4,7 @@ cd /home/centos/WebApp/
 # use systemd to start and monitor dotnet application
 sudo systemctl unmask kestrel.service
 sudo systemctl status kestrel.service
-sudo systemctl restart kestrel.service
+sudo systemctl start kestrel.service
 
 sudo systemctl status kestrel.service
 
