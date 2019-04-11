b8ae62365b73c683f52003404c2767021bd779d2
Shaan0607 <deosthale.s@northeastern.edu>
2019-04-10 21:19:01 -0400

    HI

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo rm -rf /home/centos/WebApp/logs
+sudo systemctl stop kestrel.service
 sudo systemctl start kestrel.service
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/WebApp/cloudwatch-config.json -s
