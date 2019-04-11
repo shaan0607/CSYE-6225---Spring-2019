b08371a5b503b995e052bc05cad1f9116ed04660
Shaan0607 <deosthale.s@northeastern.edu>
2019-04-10 20:38:36 -0400

    HI

--- webapp/NoteApp_Production/Controllers/ValuesController.cs
+++ webapp/NoteApp_Production/Controllers/ValuesController.cs
@@ -256,7 +256,7 @@ namespace trial.Controllers
             return StatusCode(200, Json);
             }
             else{
-                return StatusCode(200, new{result = "You Don't have any notes"});
+                return StatusCode(200, new{result = "You Don't have any notes!!! Sorry"});
             }
        }
         [HttpGet]
